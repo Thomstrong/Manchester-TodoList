9680f155e33a4a5fdd4674ca6e50b445a5863992
Thomstrong <a80090@qq.com>
2018-09-15 14:12:10 +0800

    make id generate automatic and set time zone

--- TodoApp/serializers.py
+++ TodoApp/serializers.py
@@ -1,8 +1,23 @@
+import uuid
+from datetime import datetime
+
+import pytz
 from rest_framework import serializers
+
 from TodoApp.models import TodoListItem
 
 
 class TodoListItemSerializer(serializers.HyperlinkedModelSerializer):
+    restTime = serializers.SerializerMethodField("getRestTime")
+
     class Meta:
         model = TodoListItem
-        fields = ('id', 'description', 'deadline', 'priority', 'status', 'url',)
+        fields = ('id', 'description', 'deadline', 'restTime', 'priority', 'status', 'url',)
+
+    def save(self, *args, **kwargs):
+        if not self.object.id:
+            self.object.id = str(uuid.uuid4())
+        super(TodoListItem, self.object).save(*args, **kwargs)
+
+    def getRestTime(self, instance):
+        return instance.deadline - datetime.now(tz=pytz.timezone('Asia/Shanghai'))
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -11,17 +11,3 @@ from TodoApp.serializers import TodoListItemSerializer
 class TodoListItem(viewsets.ModelViewSet):
     serializer_class = TodoListItemSerializer
     queryset = TodoListItem.objects.all()
-
-    def create(self, request, *args, **kwargs):
-        request.DATA['id'] = str(uuid.uuid4())
-        serializer = self.get_serializer(data=request.DATA, files=request.FILES)
-
-        if serializer.is_valid():
-            self.pre_save(serializer.object)
-            self.object = serializer.save(force_insert=True)
-            self.post_save(self.object, created=True)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED,
-                            headers=headers)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
